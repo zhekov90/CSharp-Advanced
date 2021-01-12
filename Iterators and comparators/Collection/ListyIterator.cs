2aa8d24542abbf0ccfbaefd42f3c187a232e0b1f
Zdravko Zhekov <63153601+zhekov90@users.noreply.github.com>
2021-01-13 00:55:37 +0200

    Add files via upload

---
+++ Iterators and comparators/Collection/ListyIterator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private const string ExceptionMessage = "Invalid Operation!";
+    public int CurrentIndex { get; private set; }
+    private readonly IList<T> dataList;
+
+    public ListyIterator(params T[] items)
+    {
+        this.CurrentIndex = 0;
+        this.dataList = new List<T>(items);
+    }
+
+    public bool Move()
+    {
+        if (this.CurrentIndex < this.dataList.Count - 1)
+        {
+            ++this.CurrentIndex;
+            return true;
+        }
+        return false;
+    }
+
+    public bool HasNext()
+    {
+        if (this.CurrentIndex < this.dataList.Count - 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string Print()
+    {
+        return this.dataList.Count == 0
+            ? throw new InvalidOperationException(ExceptionMessage)
+            : $"{this.dataList[this.CurrentIndex]}";
+    }
+
+    public string PrintAll()
+    {
+       
+        var sb = new StringBuilder();
+        foreach (var item in this.dataList)
+        {
+            sb.Append($"{item} ");
+        }
+        return sb.ToString().Trim();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.dataList.Count; i++)
+        {
+            yield return this.dataList[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ Iterators and comparators/Stack/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    private static string[] parameters = null;
+    private static string inputLine;
+
+    public static void Main()
+    {
+        var data = new Stack<int>();
+
+        var commands = new Dictionary<string, Action>
+        {
+            {"Push", ()=> data.Push(parameters.Select(int.Parse).ToArray()) },
+            {"Pop",() => data.Pop() }
+        };
+
+        while ((inputLine = Console.ReadLine()) != "END")
+        {
+            var commandArgs = inputLine.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var command = commandArgs[0];
+            commandArgs.RemoveAt(0);
+
+            if (commands.ContainsKey(command))
+            {
+                parameters = commandArgs.ToArray();
+                try
+                {
+                    commands[command].Invoke();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        PrintRest(data);
+    }
+
+    private static void PrintRest<T>(Stack<T> data)
+    {
+        foreach (var item in data)
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in data)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
